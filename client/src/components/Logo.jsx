037117e1a15aae33e5e0644abc1d67dd6c7cb3c1
abhijeet14d <abhijeetsinghwork.01@gmail.com>
2025-11-10 19:16:32 +0530

    made some changes

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,7 +2,7 @@ const Logo = ({ size = "text-2xl" }) => (
   <div className={`font-semibold tracking-tight ${size}`}>
     <span className="text-sky-400">Dev</span>
     <span className="text-white">Ops</span>
-    <span className="text-sky-400">Todo</span>
+    <span className="text-sky-400">TodoAPP</span>
   </div>
 );
 
